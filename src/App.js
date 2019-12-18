3abebd6b6dcfcd5f69e1ad2f49d675746ee9bac4
Diego Guajardo <diego.guajardo@23people.io>
2019-12-18 14:43:21 -0300

    pushing up the examples

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick'
-import EventBind from './components/EventBind'
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting'
-import NameList from './components/NameList'
-import Stylesheet from './components/Stylesheet'
-import Inline from './components/Inline'
-import './appStyles.css'
-import styles from './appStyles.module.css'
-
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome'
+// import Hello from './components/Hello'
+// import Message from './components/Message'
+// import Counter from './components/Counter'
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick'
+// import EventBind from './components/EventBind'
+// import ParentComponent from './components/ParentComponent';
+// import UserGreeting from './components/UserGreeting'
+// import NameList from './components/NameList'
+// import Stylesheet from './components/Stylesheet'
+// import Inline from './components/Inline'
+// import './appStyles.css'
+// import styles from './appStyles.module.css'
+// import Form from './components/Form'
+import LifecycleA from './components/LifecycleA'
+// import Example from './components/Example'
+//import FriendStatus from './components/FriendStatus'
 class App extends Component {
     render(){
         return (
                 < div className = "App" >
-                <h1 className='error'>Error</h1>
-                <h1 className={styles.succes}>Succes</h1>
+                {/* <FriendStatus/> */}
+                {/* <Example/> */}
+                <LifecycleA/>
+                {/*   */}
+                {/* <h1 className='error'>Error</h1>
+                <h1 className={styles.succes}>Succes</h1> */}
                 {/* <Inline/> */}
                 {/* <Stylesheet primary = {true}/> */}
                 {/* <NameList/> */}
---
+++ src/components/LifecycleA.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import LifecycleB from './LifecycleB'
+
+class LifecycleA extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            name: 'Diego'
+        }
+        console.log('LifecycleA constructor')
+    }
+    
+    static getDerivedStateFromProps(props, state) {
+        console.log('LifecycleA getDerivedStateFromProps')    
+        return null    
+    }
+
+
+    componentDidMount() {
+        console.log('LifecycleA componentDidMount')
+    }
+    shouldComponentUpdate() { 
+        console.log('LifecycleA shouldComponentUpdate')
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('LifecycleA getSnapShotBeforeUpdate')
+        return null
+    }
+
+    componentDidUpdate() {
+        console.log('LifecycleA componentDidUpdate')
+    }
+
+    changeState = () =>{
+        this.setState({
+            name: 'Codeevolution'
+        })
+    }
+
+    render() {
+        console.log('LifecycleA render')
+        return (
+          <div>
+            <div>LifecycleA</div>
+            <button onClick={this.changeState}>Change state</button>
+            <LifecycleB/>
+          </div>
+        )
+    }
+}
+
+export default LifecycleA
